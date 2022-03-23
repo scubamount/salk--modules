aef2dc61ccf98c9fd3a050687d9444690b2ce4ee
dotyerts <59990200+dotyerts@users.noreply.github.com>
2022-03-23 13:51:40 -0700

    removed uneeded

--- mymodules/Microsoft.PowerShell.Operation.Validation/1.0.1/Test/Modules/Example1.Diagnostics/Diagnostics/Simple/Example1.Diagnostics.Tests.ps1
+++
@@ -1,7 +0,0 @@
-Describe "Example diagnostic finds the proper application" {
-    It "Finds built in command get-Command" {
-        $CommandName = "Get-Command"
-        $cmd = Get-Command Get-Command
-        $Cmd.Name | Should Be $CommandName
-    }
-}
